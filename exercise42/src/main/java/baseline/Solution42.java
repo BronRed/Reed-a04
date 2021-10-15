2a89312d3274ae20c2db2f969c19258660d2b950
Brian <Brian.reed1202@gmail.com>
2021-10-15 17:24:23 -0400

    added pseudocode for ex42

---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,14 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Brian Reed
+ */
+package baseline;
+
+public class Solution42 {
+    public static void main(String[] args){
+        //create employee object with  last first and salary
+        //while input file has next line, get data for employee
+        //print tab header "Last First Salary\n-------------------"
+        //print out employee data in tabular format
+    }
+}
