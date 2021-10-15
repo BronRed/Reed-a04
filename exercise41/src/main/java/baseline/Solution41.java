205401b2b791600b0fd8226d6305475332b8fe1a
Brian <Brian.reed1202@gmail.com>
2021-10-15 15:29:06 -0400

    added pseudocode for ex41

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,12 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Brian Reed
+ */
+package baseline;
+
+public class Solution41 {
+    //read data in input.txt until no more lines
+    //store names in arraylist
+    //sort names alphabetically
+    //output names using filewriter
+}
