3307979a4bdc791685978913270cb58fcdb8391c
Brian <Brian.reed1202@gmail.com>
2021-10-16 17:03:53 -0400

    ex43 pseudocode

---
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,19 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Brian Reed
+ */
+package baseline;
+
+public class Solution43 {
+    public static void main(String[] args){
+        //prompt for site name
+        //prompt for author name
+        //prompt for JavaScript file
+        //prompt for CSS file
+        //get path
+        //create website folder
+        //create JS folder
+        //create CSS folder
+        //create HTML file
+    }
+}
