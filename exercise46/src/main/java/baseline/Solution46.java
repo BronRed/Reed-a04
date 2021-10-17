9bc2a094f8668eb921babc756c3bda4277834b87
Brian <Brian.reed1202@gmail.com>
2021-10-17 18:04:01 -0400

    ex46 pseudocode

---
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -0,0 +1,14 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Brian Reed
+ */
+package baseline;
+
+public class Solution46 {
+    public static void main(String[] args){
+        //hashmap with word and # times it was used
+        //read through file and add words to hashmap
+        //count number of times a word has been used in hashmap
+        //print out hashmap
+    }
+}
