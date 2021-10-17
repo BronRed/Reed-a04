8c752299124ca6394768df06c6ef865b05110a81
Brian <Brian.reed1202@gmail.com>
2021-10-17 16:02:56 -0400

    ex45 pseudocode

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,13 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Brian Reed
+ */
+package baseline;
+
+public class Solution45 {
+    public static void main(String[] args){
+        //Create output file
+        //read in input file and replace "utilize" with "use"
+        //copy new text to output file
+    }
+}
